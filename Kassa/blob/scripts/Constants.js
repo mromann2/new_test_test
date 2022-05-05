a7106f82515e86a53f6c9dcdc17e16f053649a7f
Меджидов Роман Нурбабанович <mromann@inbox.ru>
2022-05-05 16:38:38 +0300

    1st commit

---
+++ Kassa/blob/scripts/Constants.js
@@ -0,0 +1,6 @@
+const MODULEMNEMO = 'Kassa';
+
+const PostingMethods = {setProperty: "setProperty", 
+                        setDebit: "setDebit", setDebitProperty: "setDebitProperty",
+                        setCredit: "setCredit", setCreditProperty: "setCreditProperty",
+                        setValue: "setValue", setComment: "setComment", setJournals: "setJournals"}
