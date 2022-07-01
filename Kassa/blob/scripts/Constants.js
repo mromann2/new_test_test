540af661cf277dd856249d2e22f382816863325d
mromann2 <mromann@inbox.ru>
2022-07-01 13:31:00 +0300

    delete

--- Kassa/blob/scripts/Constants.js
+++
@@ -1,6 +0,0 @@
-const MODULEMNEMO = 'Kassa';
-
-const PostingMethods = {setProperty: "setProperty", 
-                        setDebit: "setDebit", setDebitProperty: "setDebitProperty",
-                        setCredit: "setCredit", setCreditProperty: "setCreditProperty",
-                        setValue: "setValue", setComment: "setComment", setJournals: "setJournals"}
